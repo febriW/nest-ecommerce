242bde99c73c7af0542a115f683c6d8e44596f68
febriW <febriwanda26@gmail.com>
2023-08-16 00:17:12 +0700

    added TypeORM and first try controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('number')
+  getNumber(): Array<Number> {
+    return this.appService.getNumber();
+  }
+
+  @Get('number/:id')
+  findOne(@Param() params:any): string {
+    return `The Number is ${params.id}`
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'string' ;
+  }
+  getNumber(): Array<Number> {
+    return [1,2,3]
   }
 }
+
