634311904ddef0ef9924b2a70b238296b526c59c
febriw <febriwanda26@gmail.com>
2024-12-02 16:34:38 +0700

    pack all modules to index file on root modules folder

---
+++ src/modules/index.ts
@@ -0,0 +1,9 @@
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { RolesModule } from './roles/roles.module';
+
+export const ListModules = [
+    UsersModule,
+    AuthModule,
+    RolesModule
+]
